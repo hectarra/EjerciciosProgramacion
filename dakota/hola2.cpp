b8527a3a7e2acb26226df4b46fbd6bdaa70138f8
nesiines73 <inprieto@ucm.es>
2017-10-23 22:40:05 +0200

    hola

---
+++ dakota/hola2.cpp
@@ -0,0 +1,11 @@
+// Strings
+#include <iostream>
+#include <string>
+usin namespace std;
+main (){
+	string nombre;
+	cout << "introduce tu nombre: ";
+	cin >> nombre;
+	cout << "hola " << nombre;
+  return 0;
+}
